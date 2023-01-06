e5fd429acc8b9363c16712cacd90f371626f921f
Pranjul khankriyal <109353834+Pranjul2002@users.noreply.github.com>
2023-01-06 18:51:38 +0530

    Array and its basic Operations.

---
+++ Array/staticArray.java
@@ -0,0 +1,10 @@
+public class staticArray{
+    public static void main(String[] args) {
+        int arr[]={20,40,60,80};
+        System.out.println("Array is- ");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println("");
+    }
+}
