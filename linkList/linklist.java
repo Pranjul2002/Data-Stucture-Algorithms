1de15a68bb5242715919d4188418b8b115f556e9
Pranjul khankriyal <109353834+Pranjul2002@users.noreply.github.com>
2023-01-07 00:28:53 +0000

    Adding operation-addlast and print the link-list.

--- linkList/linklist.java
+++ linkList/linklist.java
@@ -21,12 +21,44 @@ public class linklist{
         return;
     }
 
+    public void addLast(int data){
+        node newNode=new node(data);
+        if(front==null){
+            front=newNode;
+            return;
+        }
+        node temp=front;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=newNode;
+        return;
+    }
+    
+    public void printlist(){
+        if(front==null){
+            System.out.println("Empty list");
+            return;
+        }
+        node temp=front;
+        while(temp.next!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("NULL");
+        return;
+    }
+
     public static void main(String[] args) {
         linklist ll=new linklist();
         ll.addFirst(2);
         ll.addFirst(4);
         ll.addFirst(6);
         ll.addFirst(8);
+        ll.addLast(10);
+        ll.addLast(12);
+        ll.addLast(14);
         System.out.println("front element is- "+front.data);
+        ll.printlist();
     }
 }
