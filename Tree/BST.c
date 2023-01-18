e4eaeb0a0d1f73bf340a80cb25179d736a9b0f3a
Pranjul khankriyal <109353834+Pranjul2002@users.noreply.github.com>
2023-01-18 21:02:11 +0530

    Update BST.c

--- Tree/BST.c
+++ Tree/BST.c
@@ -1,49 +1,85 @@
+//Binary search tree and Tree Traversal
+
 #include<stdio.h>
 #include<stdlib.h>
 
 struct node{
-	int data;
-	struct node *left;
-	struct node *right;
+    int data;
+    struct node* right;
+    struct node* left;
 }*root=NULL;
 
-struct node *createNode(int data){
-	struct node *newNode=(struct node *)malloc(sizeof(struct node));
-	newNode->data=data;
-	newNode->left=NULL;
-	newNode->right=NULL;
-	return newNode;
+//inorder traversal
+void inorderTravese(struct node *root){
+    if(root==NULL){
+        return;
+    }
+    inorderTravese(root->left);
+    printf("%d ",root->data);
+    inorderTravese(root->right);
 }
 
-struct node *insert(int data){
-	struct node *newNode=createNode(data);
-	if(root==NULL){
-		root=newNode;
-		return;
-	}
-	struct node *current=root;
-	struct node *parent=NULL;
-	while(current!=NULL){
-		parent=current;
-		if(data<=current->data){
-			current=current->left;
-		}else{
-			current=current->right;
-		}
-	}
-	if(data<=parent->data){
-		root->left=newNode;
-	}else if(data>parent->data){
-		root->right=newNode;
-	}
-	return root;
+//preorder traversal
+
+void preorderTraverse(struct node *root){
+    if(root==NULL){
+        return;
+    }
+    preorderTraverse(root->left);
+    preorderTraverse(root->right);
+    printf("%d ",root->data);
+}
+
+//postorder traversal
+
+void postorderTraverse(struct node *root){
+    if(root==NULL){
+        return;
+    }
+    printf("%d ",root->data);
+    postorderTraverse(root->left);
+    postorderTraverse(root->right);
+}
+
+void insert(int data){
+    struct node *newNode=malloc(sizeof(struct node));
+    newNode->data=data;
+    newNode->right=NULL;
+    newNode->left=NULL;
+    if(root==NULL){
+        root=newNode;
+        return;
+    }
+    struct node *current=root;
+    struct node *parent=NULL;
+    while (current!=NULL){
+        parent=current;
+        if(data<=current->data){
+            current=current->left;
+        }else{
+            current=current->right;
+        }
+    }
+    if(data<=parent->data){
+        parent->left=newNode;
+        return;
+    }else{
+        parent->right=newNode;
+        return;
+    }
 }
 
 int main(){
-	printf("\nenter a number you want to insert-");
-	int data;
-	scanf("%d",&data);
-	insert(data);
-	return 0;
+    int arr[]={15,20,30,10,25,5};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<size;i++){
+        insert(arr[i]);
+    }
+    inorderTravese(root);
+    printf("\n");
+    preorderTraverse(root);
+    printf("\n");
+    postorderTraverse(root);
+    printf("\n");
+    return 0;
 }
-
